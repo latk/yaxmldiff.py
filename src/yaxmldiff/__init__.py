608421f6937ed90d167dadadfac529610fdd0d46
Lukas Atkinson <opensource@LukasAtkinson.de>
2026-07-18 12:37:06 +0200

    bump copyright year to 2026

--- src/yaxmldiff/__init__.py
+++ src/yaxmldiff/__init__.py
@@ -1,5 +1,5 @@
 # yaxmldiff is Yet Another XML Differ <https://github.com/latk/yaxmldiff.py>
-# Copyright 2021-2024 Lukas Atkinson
+# SPDX-FileCopyrightText: 2021-2026 Lukas Atkinson
 # SPDX-License-Identifier: Apache-2.0
 
 """yaxmldiff is Yet Another XML Differ <https://github.com/latk/yaxmldiff.py>.
